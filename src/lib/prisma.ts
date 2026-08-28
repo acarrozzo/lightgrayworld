2a9fe188ce4c26ef2288afed5a8bbfe7ecbad11b
Anthony Carrozzo <acarrozzo@gmail.com>
2026-08-28 16:58:37 -0400

    LGU-v0.1.6-forestepic-fly deploy fixB

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,14 @@
 import type { PrismaClient } from '@prisma/client'
-import dbClient from './db-client'
 
-export const prisma = dbClient.prisma as PrismaClient
+let _prisma: PrismaClient | undefined
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, prop) {
+    if (!_prisma) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const dbClient = require('./db-client')
+      _prisma = dbClient.prisma as PrismaClient
+    }
+    return (_prisma as Record<string | symbol, unknown>)[prop]
+  },
+})
